bb0f8a1422c162640d6ccfc4d95f69865bef8a5d
Psyh2409 <psyh2409@gmail.com>
2023-11-06 13:56:37 +0200

    Rename class

--- Java_Core_5/src/com/gmail/psyh2409/thisConstructor/ConstructorsTestClass.java
+++ Java_Core_5/src/com/gmail/psyh2409/thisConstructor/ConstructorsTestClass.java
@@ -1,18 +1,18 @@
 package com.gmail.psyh2409.thisConstructor;
 
-public class ThisConstructor {
+public class ConstructorsTestClass {
 	 public int i;
 	 public double d;
 	 
-	 public ThisConstructor(int i, double d){
+	 public ConstructorsTestClass(int i, double d){
 	  this.i=i;
 	  this.d=d;
 	  }
-	 public ThisConstructor(int i){
+	 public ConstructorsTestClass(int i){
 	  this(i, 3.14);
 	 }
 	 public static void main(String[] args) {
-		 ThisConstructor m =  new ThisConstructor(5);
+		 ConstructorsTestClass m =  new ConstructorsTestClass(5);
 	  System.out.println(m.d);
 	 }
 	 //Output: 3.14
